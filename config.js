67a64d37d10a7436bd0d3ce14260568cf0b6bf3a
51sec <johnyan.ca@gmail.com>
2021-01-10 16:35:23 -0500

    Update config.js

--- config.js
+++ config.js
@@ -10,10 +10,10 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'm783399407-b5654044806678e2a377de7f',
+    'm783399409-ec794f627ab64b259503f034',
+    'm786883632-b151d0e9e9796886c2b9c390',
+   
   ],
 
   // 是否显示监测站点的链接
@@ -22,17 +22,17 @@ window.Config = {
   // 日志天数
   // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
   // 不过时间不要设置太长，容易卡，接口请求也容易失败
-  CountDays: 60,
+  CountDays: 90,
 
   // 导航栏菜单
   Navi: [
     {
       text: 'Homepage',
-      url: 'https://status.org.cn/'
+      url: 'https://51sec.org/'
     },
     {
       text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
+      url: 'https://github.com/51sec/uptime-status'
     }
   ]
 };
