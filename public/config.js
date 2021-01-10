a577f46e5fd9a4404778ddae9a37704565fead7f
51sec <johnyan.ca@gmail.com>
2021-01-10 15:59:26 -0500

    Add files via upload

--- public/config.js
+++ public/config.js
@@ -1,14 +1,14 @@
-// 配置 - Configuration
+// 配置
 window.Config = {
 
-  // 站点名 - Site Name
+  // 站点名
   SiteName: 'Uptime Status',
 
-  // 站点链接 - Site URL
+  // 站点链接
   SiteUrl: '/',
 
   // UptimeRobot Api Keys
-  // 支持 Monitor-Specific 和 Read-Only 两只 Api Key - Support two different API Keys: Monitor-Specific and Read-Only
+  // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
     'm784488775-dd1ad84b209c05f8e185c33e',
     'm784490063-7b5da437e7f1e0d67613714d',
@@ -16,15 +16,15 @@ window.Config = {
     'm784496436-71a4bf7b1e3bdf7756be131b',
   ],
 
-  // 是否显示监测站点的链接 - If show the URL for monitored sites
+  // 是否显示监测站点的链接
   ShowLink: true,
 
-  // 日志天数 - Log Days
+  // 日志天数
   // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
   // 不过时间不要设置太长，容易卡，接口请求也容易失败
   CountDays: 60,
 
-  // 导航栏菜单 - Navigation Menu
+  // 导航栏菜单
   Navi: [
     {
       text: 'Homepage',
