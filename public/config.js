e6653af39a4daaffcad7ce0bf4496d201ff25e17
51sec <johnyan.ca@gmail.com>
2021-01-09 22:49:40 -0500

    Update config.js
    Translated to English

--- public/config.js
+++ public/config.js
@@ -1,14 +1,14 @@
-// 配置
+// 配置 - Configuration
 window.Config = {
 
-  // 站点名
+  // 站点名 - Site Name
   SiteName: 'Uptime Status',
 
-  // 站点链接
+  // 站点链接 - Site URL
   SiteUrl: '/',
 
   // UptimeRobot Api Keys
-  // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
+  // 支持 Monitor-Specific 和 Read-Only 两只 Api Key - Support two different API Keys: Monitor-Specific and Read-Only
   ApiKeys: [
     'm784488775-dd1ad84b209c05f8e185c33e',
     'm784490063-7b5da437e7f1e0d67613714d',
@@ -16,15 +16,15 @@ window.Config = {
     'm784496436-71a4bf7b1e3bdf7756be131b',
   ],
 
-  // 是否显示监测站点的链接
+  // 是否显示监测站点的链接 - If show the URL for monitored sites
   ShowLink: true,
 
-  // 日志天数
+  // 日志天数 - Log Days
   // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
   // 不过时间不要设置太长，容易卡，接口请求也容易失败
   CountDays: 60,
 
-  // 导航栏菜单
+  // 导航栏菜单 - Navigation Menu
   Navi: [
     {
       text: 'Homepage',
