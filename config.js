1e7ae779a6ccf42459668bf239911554f7ec4e98
51sec <johnyan.ca@gmail.com>
2021-10-16 12:25:51 -0400

    Update config.js

--- config.js
+++ config.js
@@ -16,10 +16,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // Support both Monitor-Specific and  Read-Only type's Api Key
   ApiKeys: [
-    'm783399407-b5654044806678e2a377de7f',
-    'm783399409-ec794f627ab64b259503f034',
-    'm786883632-b151d0e9e9796886c2b9c390',
-    'm783491939-22f42834a494fc3eadf69059',
+    'u488665-9b30cfe3bfabfef00d6d3734',
   ],
 
   // if shows monitored site's URL
