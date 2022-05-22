65426434e8d2c257540c8ec297aff0a1111bf478
uselessXfrosty <76498086+uselessXfrosty@users.noreply.github.com>
2022-05-22 07:32:42 -0400

    Update config.js

--- config.js
+++ config.js
@@ -19,6 +19,7 @@ window.Config = {
     'm783399407-b5654044806678e2a377de7f',
     'm783399409-ec794f627ab64b259503f034',
     'm783491939-22f42834a494fc3eadf69059',
+    'm791827461-9b62947f2a254b6be1b21b3f',
   ],
 
   // if shows monitored site's URL
