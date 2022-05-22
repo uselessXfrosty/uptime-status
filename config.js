f3bf1775520166c1e81fe01d705daf0b334bfb27
uselessXfrosty <76498086+uselessXfrosty@users.noreply.github.com>
2022-05-22 07:27:03 -0400

    Update config.js

--- config.js
+++ config.js
@@ -16,9 +16,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // Support both Monitor-Specific and  Read-Only type's Api Key
   ApiKeys: [
-    'm783399407-b5654044806678e2a377de7f',
-    'm783399409-ec794f627ab64b259503f034',
-    'm783491939-22f42834a494fc3eadf69059',
+    'm791827461-9b62947f2a254b6be1b21b3f',
   ],
 
   // if shows monitored site's URL
